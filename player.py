1b6929ecb8b5f9b252f675620dbb2bd9b0f923e5
MarcoReguillot <marco.reguillot@epitech.eu>
2021-01-29 21:25:45 +0100

    [ADD] fluid player rotation

--- player.py
+++ player.py
@@ -3,6 +3,20 @@
 
 import pygame
 import animations
+import random
+
+def get_nearest(value, goal1, goal2):
+    if (value == goal1 or value == goal2):
+        return (value)
+    if (abs(goal1 - value) == abs(goal2 - value)):
+        if (random.randint(0, 1) == 0):
+            return(goal1)
+        else:
+            return (goal2)
+    elif (abs(goal1 - value) > abs(goal2 - value)):
+        return (goal2)
+    else:
+        return (goal1)
 
 class Player(pygame.sprite.Sprite):
 
@@ -21,6 +35,7 @@ class Player(pygame.sprite.Sprite):
         self.x = 0
         self.y = 0
         self.speed = 10
+        self.rotation_speed = 20
         self.actual_speed = [0, 0]
         self.stop_speed = 1.5
         self.current_image = self.idle
@@ -47,29 +62,38 @@ class Player(pygame.sprite.Sprite):
     def go_down(self, speed):
         self.actual_speed[1] = speed
 
-    def decelerate(self, speed):
-        if (speed > 0):
-            speed -= self.stop_speed
-            if (speed < 0):
-                speed = 0
-        elif (speed < 0):
-            speed += self.stop_speed
-            if (speed > 0):
-                speed = 0
-        return (speed)
+    def go_to_value(self, value, step, goal):
+        if (value > goal):
+            value -= step
+            if (value < goal):
+                value = goal
+        elif (value < goal):
+            value += step
+            if (value > goal):
+                value = goal
+        return (value)
 
     def handle_movements(self, keys_pressed):
         movements = []
         y = False
         x = False
+        print(self.rotation)
         if keys_pressed()[pygame.K_UP]:
-            self.rotation = 0
+            self.rotation = self.go_to_value(self.rotation, self.rotation_speed, get_nearest(self.rotation, 0, 360))
+            if (self.rotation == 360):
+                self.rotation = 0
         elif keys_pressed()[pygame.K_RIGHT]:
-            self.rotation = -90
+            self.rotation = self.go_to_value(self.rotation, self.rotation_speed, get_nearest(self.rotation, 270, -90))
+            if (self.rotation == -90):
+                self.rotation = 270
         elif keys_pressed()[pygame.K_LEFT]:
-            self.rotation = 90
+            self.rotation = self.go_to_value(self.rotation, self.rotation_speed, get_nearest(self.rotation, 90, -270))
+            if (self.rotation == -270):
+                self.rotation = 90
         elif keys_pressed()[pygame.K_DOWN]:
-            self.rotation = 180
+            self.rotation = self.go_to_value(self.rotation, self.rotation_speed, get_nearest(self.rotation, 180, -180))
+            if (self.rotation == -180):
+                self.rotation = 180
         for i in self.controls:
             if keys_pressed()[i[0]]:
                 if (i[0] == pygame.K_UP or i[0] == pygame.K_DOWN):
@@ -80,6 +104,6 @@ class Player(pygame.sprite.Sprite):
         for i in movements:
             i(self.speed / len(movements))
         if (x == False):
-            self.actual_speed[0] = self.decelerate(self.actual_speed[0])
+            self.actual_speed[0] = self.go_to_value(self.actual_speed[0], self.stop_speed, 0)
         if (y == False):
-            self.actual_speed[1] = self.decelerate(self.actual_speed[1])
+            self.actual_speed[1] = self.go_to_value(self.actual_speed[1], self.stop_speed, 0)
