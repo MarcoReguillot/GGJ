ae92065b9c930e92ab6d97c1c0b72b04d60eae3d
MarcoReguillot <marco.reguillot@epitech.eu>
2021-01-30 23:28:45 +0100

    [ADD] new in-game props

--- objects.py
+++ objects.py
@@ -6,6 +6,7 @@
 ##
 
 import pygame
+import math
 
 class InteractiveObject(pygame.sprite.Sprite):
     def __init__(self, image, position, scale, text):
@@ -55,24 +56,49 @@ class DecoObject(pygame.sprite.Sprite):
     def update(self, screen, player):
         screen.blit(self.image, (self.x - player.x, self.y - player.y))
 
+
 class Objects(pygame.sprite.Sprite):
+
+    def lamp(self, pos, scale, angle):
+        self.deco_objects.append(DecoObject(self.images['deco_lamp'], pos, scale, angle))
+        self.solid_objects.append(SolidObject(self.images['solid_lamp'],
+            (round(pos[0] + 0.2 * scale[0]), round(pos[1] + 0.2 * scale[1])),
+            (round(scale[0] * 0.6), round(scale[1] * 0.6)), angle))
+
     def __init__(self, world):
         self.solid_objects = []
+        self.deco_objects = []
 
         if (world == 0): #bright
             self.images = {
-                'siege': pygame.image.load("assets/props/pos/solid/siege.png")
+                'siege': pygame.image.load("assets/props/pos/solid/siege.png"),
+                'solid_lamp': pygame.image.load("assets/props/pos/solid/lamp.png"),
+                'deco_lamp': pygame.image.load("assets/props/pos/deco/lamp.png"),
+                'os': pygame.image.load("assets/props/pos/deco/os.png")
             }
             self.solid_objects.append(SolidObject(self.images['siege'], (200, 200), (140, 120), -95))
             self.solid_objects.append(SolidObject(self.images['siege'], (200, 400), (140, 120), -85))
             self.solid_objects.append(SolidObject(self.images['siege'], (850, 650), (140, 120), 4))
+
+            self.deco_objects.append(DecoObject(self.images['os'], (800, 400), (50, 80), 120))
+
+            self.lamp((600, 400), (100, 100), 0)
         else:
             self.images = {
-                'siege': pygame.image.load("assets/props/neg/solid/siege.png")
+                'siege': pygame.image.load("assets/props/neg/solid/siege.png"),
+                'solid_lamp': pygame.image.load("assets/props/neg/solid/lamp.png"),
+                'deco_lamp': pygame.image.load("assets/props/neg/deco/lamp.png"),
+                'os': pygame.image.load("assets/props/neg/deco/os.png")
             }
             self.solid_objects.append(SolidObject(self.images['siege'], (200, 200), (140, 120), -95))
             self.solid_objects.append(SolidObject(self.images['siege'], (200, 400), (140, 120), -85))
             self.solid_objects.append(SolidObject(self.images['siege'], (850, 650), (140, 120), 184))
+
+            self.deco_objects.append(DecoObject(self.images['os'], (800, 400), (50, 80), 120))
+
+            self.lamp((600, 400), (100, 100), 0)
     def update(self, screen, player):
+        for i in self.deco_objects:
+            i.update(screen, player)
         for i in self.solid_objects:
             i.update(screen, player)
