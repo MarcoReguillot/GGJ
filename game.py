373bf9f5dc17e2e4582bae416eb8aa5ad7ce9fd3
Johann Lucas Sako <SevenNox@localhost.localdomain>
2021-01-30 13:29:22 +0100

    [ADD] collision

--- game.py
+++ game.py
@@ -48,12 +48,14 @@ def collide(character, obj):
 def change_space(space):
     if space == 0:
         maps.old(player.x, player.y)
+        player.handle_collisions(maps.Rect_black)
         for inc in range(len(maps.Rect_white)):
             if collide(player.rect, maps.Rect_white[inc]) == 1:
                 return (1)
         return (0)
     else:
         maps.child(player.x, player.y)
+        player.handle_collisions(maps.Rect_white)
         for inc in range(len(maps.Rect_black)):
             if collide(player.rect, maps.Rect_black[inc]) == 1:
                 return (1)
